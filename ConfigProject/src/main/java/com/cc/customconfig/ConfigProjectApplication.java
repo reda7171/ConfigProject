ebe3fc2c9b41a9e115851bedd32f8e95040b6027
redaa <redaa@AZIZI>
2024-10-28 09:22:51 +0100

    First Commit

---
+++ ConfigProject/src/main/java/com/cc/customconfig/ConfigProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cc.customconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigProjectApplication.class, args);
+	}
+
+}
